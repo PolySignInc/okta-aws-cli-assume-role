b89554905af2b1bf745729d86569219362dab2d5
Arun Velagapalli <av82.msstate@gmail.com>
2020-02-23 11:38:36 -0800

    Update install.sh
    hardcode latest known url for release version

--- bin/install.sh
+++ bin/install.sh
@@ -80,9 +80,7 @@ PREFIX="$(cd -P -- "${PREFIX}" && pwd)"
 echo "Installing into ${PREFIX}" | sed "s#$HOME#~#g"
 
 mkdir -p ${PREFIX}
-releaseUrl=$(curl --head --silent ${repo_url}/releases/latest | grep "Location:" | cut -c11-)
-releaseTag=$(echo $releaseUrl | awk 'BEGIN{FS="/"}{print $8}' | tr -d '\r')
-url=${repo_url}/releases/download/${releaseTag}/okta-aws-cli-${releaseTag:1}.jar
+url=https://github.com/PolySignInc/okta-aws-cli-assume-role/releases/download/v2.0.4/okta-aws-cli-2.0.4.jar
 dest=${PREFIX}/$(basename ${url})
 echo "Latest release JAR file: ${url}"
 echo "Fetching JAR file → ${dest}" | sed "s#$HOME#~#g"
